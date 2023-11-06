dfcade186134352ac6e1b26f140d3e9b1769941f
thiago <thiago@dreamcode.io>
2023-11-06 19:15:20 -0300

    add common hooks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function randomBetween(from: number, to: number) {
+  return Math.floor(Math.random() * (to - from)) + from;
 }
