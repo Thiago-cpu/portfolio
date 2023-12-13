cd737800c565ccef2a4706c3fb368783d83c7dac
thiago <thiago@dreamcode.io>
2023-12-13 05:23:57 -0300

    in project section, add responsive design

--- src/app/[locale]/(project)/projectCarousel.tsx
+++ src/app/[locale]/(project)/projectCarousel.tsx
@@ -27,7 +27,7 @@ export function ProjectCarousel({ children }: ProjectCarouselProps) {
   };
 
   return (
-    <div className="bg-netbook flex h-[370px] w-[640px] bg-cover px-[105px] pb-[96px] pt-[16px]">
+    <div className="bg-netbook flex h-[208px] w-[360px] bg-cover px-[59px] pb-[54px] pt-[9px] sm:h-[370px] sm:w-[640px] sm:px-[105px] sm:pb-[96px] sm:pt-[16px]">
       <div className="pointer-events-auto relative flex grow items-center justify-between overflow-hidden bg-background">
         {items.map((item, i) => (
           <div
@@ -46,19 +46,19 @@ export function ProjectCarousel({ children }: ProjectCarouselProps) {
         {shouldShowButtons && (
           <Button
             variant="outline"
-            className="absolute left-2  rounded-full px-2"
+            className="absolute left-2 h-fit rounded-full p-1 sm:p-2"
             onClick={() => moveIndex(-1)}
           >
-            <ChevronLeft className=" h-6 w-6 shrink-0 transition-transform duration-200" />
+            <ChevronLeft className="h-5 w-5 shrink-0 transition-transform duration-200 sm:h-6 sm:w-6" />
           </Button>
         )}
         {shouldShowButtons && (
           <Button
             variant="outline"
-            className="absolute right-2 rounded-full px-2"
+            className="absolute right-2 h-fit rounded-full p-1 sm:p-2"
             onClick={() => moveIndex(1)}
           >
-            <ChevronRight className=" h-6 w-6 shrink-0 transition-transform duration-200" />
+            <ChevronRight className="h-5 w-5 shrink-0 transition-transform duration-200 sm:h-6 sm:w-6" />
           </Button>
         )}
       </div>
